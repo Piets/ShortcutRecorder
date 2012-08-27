010c8b293862d910856ee6e96c72539a3d236eb8
Peter Kraml <piets@boinx.com>
2012-08-27 10:13:39 +0200

    Fixed warnings.
    Signed-off-by: Peter Kraml <piets@boinx.com>

--- Source/SRRecorderCell.m
+++ Source/SRRecorderCell.m
@@ -236,7 +236,8 @@
 			
 		// Draw snapback image
 			NSImage *snapBackArrow = SRResIndImage(@"SRSnapback");	
-			[snapBackArrow dissolveToPoint:[self _snapbackRectForFrame: cellFrame].origin fraction:1.0f];
+//			[snapBackArrow dissolveToPoint:[self _snapbackRectForFrame: cellFrame].origin fraction:1.0f];
+			[snapBackArrow drawAtPoint:[self _snapbackRectForFrame: cellFrame].origin fromRect:NSMakeRect(0, 0, snapBackArrow.size.width, snapBackArrow.size.height) operation:NSCompositeSourceOver fraction:1.0f];
 			
 		// Because of the gradient and snapback image, the white rounded rect will be smaller
 			whiteRect = NSInsetRect(cellFrame, 9.5f, 2.0f);
@@ -262,7 +263,9 @@
 			{
 				NSString *removeImageName = [NSString stringWithFormat: @"SRRemoveShortcut%@", (mouseInsideTrackingArea ? (mouseDown ? @"Pressed" : @"Rollover") : (mouseDown ? @"Rollover" : @""))];
 				NSImage *removeImage = SRResIndImage(removeImageName);
-				[removeImage dissolveToPoint:[self _removeButtonRectForFrame: cellFrame].origin fraction:1.0f];
+//				[removeImage dissolveToPoint:[self _removeButtonRectForFrame: cellFrame].origin fraction:1.0f];
+				[removeImage drawAtPoint:[self _removeButtonRectForFrame: cellFrame].origin fromRect:NSMakeRect(0, 0, removeImage.size.width, removeImage.size.height) operation:NSCompositeSourceOver fraction:1.0f];
+
 			}
 		}
 		
@@ -451,7 +454,9 @@
 			
 		// Draw snapback image
 			NSImage *snapBackArrow = SRResIndImage(@"SRSnapback");
-			[snapBackArrow dissolveToPoint:correctedSnapBackOrigin fraction:1.0f*alphaRecording];
+//			[snapBackArrow dissolveToPoint:correctedSnapBackOrigin fraction:1.0f*alphaRecording];
+			[snapBackArrow drawAtPoint:correctedSnapBackOrigin fromRect:NSMakeRect(0, 0, snapBackArrow.size.width, snapBackArrow.size.height) operation:NSCompositeSourceOver fraction:1.0f*alphaRecording];
+
 		}
 		
 	// Draw border and remove badge if needed
@@ -463,7 +468,9 @@
 		{
 			NSString *removeImageName = [NSString stringWithFormat: @"SRRemoveShortcut%@", (mouseInsideTrackingArea ? (mouseDown ? @"Pressed" : @"Rollover") : (mouseDown ? @"Rollover" : @""))];
 			NSImage *removeImage = SRResIndImage(removeImageName);
-			[removeImage dissolveToPoint:[viewportMovement transformPoint:([self _removeButtonRectForFrame: cellFrame].origin)] fraction:alphaView];
+//			[removeImage dissolveToPoint:[viewportMovement transformPoint:([self _removeButtonRectForFrame: cellFrame].origin)] fraction:alphaView];
+			[removeImage drawAtPoint:[viewportMovement transformPoint:([self _removeButtonRectForFrame: cellFrame].origin)] fromRect:NSMakeRect(0, 0, removeImage.size.width, removeImage.size.height) operation:NSCompositeSourceOver fraction:alphaView];
+
 			//NSLog(@"drew removeImage with alpha %f", alphaView);
 		}
 //	}
